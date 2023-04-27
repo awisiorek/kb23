8d08fb31a5bcc7fe5266e465681107c0d90afe1a
aw <axel.wisiorek@lmu.de>
2023-04-27 12:05:05 +0200

    Sitzung 1 (Shell-Skripte)

---
+++ 01_shell_scripts/args.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo Skriptname: $0
+echo erstes Kommandozeilenargument: $1
+echo zweites Kommandozeilenargument: $2
+echo drittes Kommandozeilenargument: $3
+echo Index des letzten Kommandozeilenarguments: $#
+echo letztes Kommandozeilenargument: ${!#}
+echo alle Kommandozeilenargumente: "$@"
+
+
