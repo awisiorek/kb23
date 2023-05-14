d9a386af12aa6c0ebc08fd845cb61376b0e48b32
aw <axel.wisiorek@lmu.de>
2023-05-14 17:11:19 +0200

    Übungsaufgaben 2: Update Aufgabe 4

--- 02_shell_corpus/02_aufgaben/freq_list.bash
+++ 02_shell_corpus/02_aufgaben/freq_list.bash
@@ -4,4 +4,4 @@ if [[ $# != 1 ]]; then
     echo "Usage $0 FILE" && exit 1
 fi
 
-cat $1 | tr -d "[:punct:][:cntrl:]"  | tr -s " " "\n" | sort -f | uniq -ic | sort -nr #> freq.txt 
+cat $1 | tr -d "[:punct:]"  | tr -s " " "\n" | sort -f | uniq -ic | sort -nr #> freq.txt 
